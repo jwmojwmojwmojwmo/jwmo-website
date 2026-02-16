1f535047505719a53343e5b2fa1c3a4aab4cf75e
Jason Wang <jaguarjasonw61@gmail.com>
2026-02-16 00:24:36 -0800

    UNBELIEVABLE!

--- api/jeff-bot-eyes-only.js
+++ api/jeff-bot-eyes-only.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            content: "Hello World!"
+            content: `TECHNO: ${req.body.data.user.id}; UNBELIEVABLE: ${req.body.data.user.name};`
         })
     });
 
