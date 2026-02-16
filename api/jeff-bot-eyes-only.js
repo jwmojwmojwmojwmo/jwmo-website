6aa87ebcd4f3ad8232e06fdb41fa31cc0b96daac
Jason Wang <jaguarjasonw61@gmail.com>
2026-02-16 00:29:22 -0800

    ok ACTUALLY finalized

--- api/jeff-bot-eyes-only.js
+++ api/jeff-bot-eyes-only.js
@@ -9,17 +9,16 @@ export default async function handler(req, res) {
     // if (password != req.headers.authorization) {
 
     // }
-
-    const data = req.body;
     console.log(req.headers.authorization, req.body);
-
-    await fetch(discord_url, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            content: `TECHNO: ${req.body.data.user.platform_id}; UNBELIEVABLE: ${req.body.data.user.name};`
-        })
-    });
+    if (req.body.type === 'vote.create') {
+        await fetch(discord_url, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                content: `TECHNO: ${req.body.data.user.platform_id}; UNBELIEVABLE: ${req.body.data.user.name};`
+            })
+        });
+    }
 
     return res.status(200).json({ success: true });
 }
